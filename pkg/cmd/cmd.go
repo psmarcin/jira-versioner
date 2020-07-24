9bf13576317845cd7d10980d62afe719872ceb01
psmarcin <ps@psmarcin.me>
2020-07-24 18:28:14 +0200

    feat: error logs contains command output

--- pkg/cmd/cmd.go
+++ pkg/cmd/cmd.go
@@ -1,14 +1,20 @@
 package cmd
 
 import (
+	"errors"
+	"fmt"
 	"os/exec"
 )
 
 // Exec is just wrapper for exec.Command to easier mocking
 func Exec(name string, args ...string) (string, error) {
-	out, err := exec.Command(name, args...).Output()
+	c := exec.Command(name, args...)
+
+	out, err := c.CombinedOutput()
 	if err != nil {
-		return "", err
+		errMessage := fmt.Sprintf("%s: %s", err.Error(), out)
+		return "", errors.New(errMessage)
 	}
+
 	return string(out), nil
 }
